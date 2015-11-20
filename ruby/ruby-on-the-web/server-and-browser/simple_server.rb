0fad397965dc7f41e010b0d800e8f769905b5170
Cathy Douglass <cdouglass256@gmail.com>
2015-11-20 10:21:17 -0800

    Tiny browser can send GET request to simple server and get response

--- ruby/ruby-on-the-web/server-and-browser/simple_server.rb
+++ ruby/ruby-on-the-web/server-and-browser/simple_server.rb
@@ -1,11 +1,37 @@
 require 'socket'
 
+def parse(request)
+  parts = request.split
+  verb = parts[0]
+  path = "." + parts[1]
+  version = parts[2].strip
+  [verb, path, version]
+end
+
+def respond(request)
+  verb, path, version = request
+  header = ""
+  body = ""
+  case verb
+  when "GET"
+    if File.exist?(path)
+      contents = File.read(path)
+      header = "#{version} 200 OK\r\nContent-Type: text/html\r\nContent-Length: #{contents.bytesize}"
+      body = contents
+    else
+      header = "#{version} 404 File \"#{path}\" Not Found"
+    end
+  else
+  end
+  "#{header}\r\n\r\n#{body}"
+end
+
 server = TCPServer.open(2000)
 loop do
   Thread.start(server.accept) do |client|
-    client.puts(Time.now.ctime)
-    sleep(2)
-    client.puts("Closing the connection. Bye!")
+    request = client.gets
+    response = respond(parse(request))
+    client.puts(response)
     client.close
   end
 end
--- ruby/ruby-on-the-web/server-and-browser/tiny_browser.rb
+++ ruby/ruby-on-the-web/server-and-browser/tiny_browser.rb
@@ -1,15 +1,22 @@
 require 'socket'
 
-host = "www.tutorialspoint.com"
-port = 80
-path = "/index.htm"
+hostname = "localhost"
+port = 2000
+path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-socket = TCPSocket.open(host, port)
+socket = TCPSocket.open(hostname, port)
 socket.print(request)
 response = socket.read
+socket.close
 
 headers, body = response.split("\r\n\r\n", 2)
 
-print body
+status = headers.split("\r\n")[0]
+case status.split[1] # code
+when "200"
+  print body
+else
+  puts "#{status}"
+end
