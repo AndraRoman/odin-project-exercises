e3ac8629cd33e5492fac264ec437529fa567e6fc
Cathy Douglass <cdouglass256@gmail.com>
2015-12-18 16:42:13 -0800

    Forms: Bare HTML form

--- rails/re-former/config/routes.rb
+++ rails/re-former/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  get 'users/new'
+  post 'users/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
