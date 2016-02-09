e2d6340abbf02dcf5ab97af24d27b308941321fc
Cathy Douglass <cdouglass256@gmail.com>
2016-02-08 21:29:41 -0800

    Private Events: Refactor upcoming and past into scopes

--- rails/private-events/app/controllers/events_controller.rb
+++ rails/private-events/app/controllers/events_controller.rb
@@ -28,8 +28,8 @@ class EventsController < ApplicationController
   end
 
   def index
-    @past_events = Event.past
-    @upcoming_events = Event.upcoming
+    @past_events = Event.past.all
+    @upcoming_events = Event.upcoming.all
   end
 
   private
--- rails/private-events/app/models/event.rb
+++ rails/private-events/app/models/event.rb
@@ -10,12 +10,7 @@ class Event < ActiveRecord::Base
   has_many :invitations, inverse_of: :event, dependent: :destroy # singular as there's one event per invitation - is this right?
   has_many :invitees, through: :invitations, class_name: "User", inverse_of: :invited_events, source: :invitee
 
-  def Event.past
-    Event.where("start_time < ?", DateTime.now)
-  end
-
-  def Event.upcoming
-    Event.where("start_time > ?", DateTime.now)
-  end
+  scope :upcoming, -> { where("start_time > ?", DateTime.now) }
+  scope :past, -> { where("start_time < ?", DateTime.now) }
 
 end
