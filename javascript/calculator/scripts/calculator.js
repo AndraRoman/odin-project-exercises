c54cc2af634a9a149128438cdd1d5320933f365a
Cathy Douglass <cdouglass256@gmail.com>
2016-04-04 17:03:22 -0700

    JS Calculator: Working with a cheat

--- javascript/calculator/scripts/calculator.js
+++ javascript/calculator/scripts/calculator.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   var calculator = $('#calculator');
-  var current_val = 0;
+  var history = [];
+  var display = $('#display');
   make_keypad(calculator);
-  $('.key').on('click', function() {listen(this.id)}); // I'm not sure what `this` does. Or why I need to declare a function here instead of just using one.
+  $('.key').on('click', function() {listen(display, history, this.id)}); // I'm not sure what `this` does. Or why I need to declare a function here instead of just using one.
 });
 
 function make_keypad(calculator) {
@@ -10,22 +11,26 @@ function make_keypad(calculator) {
   var keys = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "CLR", "=", "/"];
   for (var i = 0; i < keys.length; i++) {
     var sym = keys[i];
-    var kind;
-    if(!isNaN(sym)) {
-      kind = "int";
-    } else if(sym == "=" || sym == "CLR") { // empty string, true, and false will look numeric but that's ok here
-      kind = "special";
-    } else {
-      kind = "operator";
-    }
-    var key = $(`<div class="key ${kind}" id="${sym}">${sym}</div>`);
+    var key = $(`<div class="key" id="${sym}"><span>${sym}</span></div>`);
     calculator.append(key);
     console.log(keys[i]);
   }
 }
 
-// TODO allow keyboard use too
+function listen(display, history, key) {
+  if(key == "CLR") {
+    history.length = 0;
+  } else if(key == "=") {
+    result = evaluate(history);
+    history.length = 0;
+    history.push(result);
+  } else {
+    history.push(key);
+  }
+  update_display(display, history);
+}
 
-function listen(val) { // TODO
-  console.log("You just clicked " + val + "!");
+function update_display(display, val) {
+  formatted = format(val);
+  display.text(formatted);
 }
--- javascript/calculator/scripts/operations.js
+++ javascript/calculator/scripts/operations.js
@@ -1,3 +1,5 @@
+// Basic operations
+
 function add(m, n) {
   var result = m + n;
   return result;
@@ -16,4 +18,22 @@ function subtract(m, n) {
 function divide(m, n) {
   var result = m / n;
   return result;
+}
+
+// Parsing
+
+// TODO
+// array of strings -> array of some integers and some strings (operator symbols)
+function parse(arr) {
+}
+
+// TODO actually implement this instead of cheating
+// takes array of numbers and operator symbols. numbers have already been grouped.
+function evaluate(arr) {
+  return eval(format(arr));
+}
+
+// String processing
+function format(arr) {
+  return arr.join("");
 }
