0a498bc8c934a51358064bf00cc6bd1ce815c31f
Cathy Douglass <cdouglass256@gmail.com>
2016-04-14 11:14:02 -0700

    Carousel: CSS navigation triangles; jQuery transition, nextSlide, and prevSlide methods

--- javascript/image-carousel/scripts/carousel.js
+++ javascript/image-carousel/scripts/carousel.js
@@ -1,9 +1,32 @@
-$(document).ready(function() {
-  "use strict";
-  $('#no-js').hide();
-  var current = $('.selected'),
-    next = current.next().hide(); // have to explicitly hide before fadeIn can work
-  [current, next].map(function (elt) {
-    elt.fadeToggle(700);
+function transition(oldSlide, newSlide) {
+  'use strict';
+  newSlide.hide(); // have to explicitly hide before fadeIn can work - 0 opacity and display: none aren't enough for jQuery to know the item can be faded in
+  [oldSlide, newSlide].map(function(elt) {
+    elt.fadeToggle('slow');
   });
+}
+
+$(document).ready(function() {
+  'use strict';
+  $('#no-js').hide();
+  var slides = $('.slide'),
+    current = $('.selected');
+  transition(current, nextSlide(slides, 0));
 });
+
+// Non-jQuery stuff below
+
+// [slide], int, int -> slide; direction will be +/- 1
+function moveInCircle(collection, index, direction) {
+  var newIndex = (index + direction) % (collection.length);
+  return collection.eq(newIndex); // to get jQuery object rather than plain DOM element
+}
+// index, [slide] -> slide
+function nextSlide(elements, startIndex) {
+  return moveInCircle(elements, startIndex, 1);
+}
+
+// index, [slide] -> slide
+function prevSlide(elements, startIndex) {
+  return moveInCircle(elements, startIndex, -1);
+}
