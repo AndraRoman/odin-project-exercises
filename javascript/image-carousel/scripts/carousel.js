da3a085c91ee6af7e1025956e1e8cf8e6ba19194
Cathy Douglass <cdouglass256@gmail.com>
2016-04-14 18:40:24 -0700

    Carousel: Automatically advance to next slide every 5 seconds

--- javascript/image-carousel/scripts/carousel.js
+++ javascript/image-carousel/scripts/carousel.js
@@ -5,16 +5,34 @@ function transition(oldSlide, newSlide) {
   });
 }
 
+function transitionTowards(slides, currentSlide, direction, timeout) {
+  var position = currentSlide.index(),
+    target = (direction < 0 ? prevSlide(slides, position) : nextSlide(slides, position));
+  timeout.restart(); // this is convenient but doesn't really belong here
+  transition(currentSlide, target);
+}
+
 $(document).ready(function() {
   'use strict';
   $('#no-js').hide();
-  var slides = $('.slide');
+  var slides = $('.slide'),
+    timeout = { 
+      delay: 5000,
+      action: function() {
+        transitionTowards(slides, $('.selected'), 1, this);
+      },
+      restart: function() {
+        window.clearTimeout(this.timeoutID);
+        this.timeoutID = window.setTimeout(() => { this.action(); }, this.delay); // arrow function lexically binds this; otherwise setTimeout would set it to the global object
+      }
+    };
+  timeout.restart();
 
   $('.arrow-container').on('click', function() {
     var current = $('.selected'),
       position = current.index(),
-      target = ($(this).attr('id') == 'left') ? prevSlide(slides, position): nextSlide(slides, position);
-    transition(current, target);
+      direction = $(this).attr('id') == 'right' ? 1 : -1;
+    transitionTowards(slides, current, direction, timeout);
   });
 });
 
