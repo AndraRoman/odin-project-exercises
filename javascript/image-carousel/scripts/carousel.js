58e60d223f1996ffb5b21fd79a5ffec952f71c44
Cathy Douglass <cdouglass256@gmail.com>
2016-04-14 19:31:35 -0700

    Carousel: Lint

--- javascript/image-carousel/scripts/carousel.js
+++ javascript/image-carousel/scripts/carousel.js
@@ -1,11 +1,33 @@
+/*jslint browser: true, white: true, es6: true, this: true*/
+/*global $, window*/
+
+// [slide], int, int -> slide; direction will be +/- 1
+function moveInCircle(collection, index, direction) {
+  'use strict';
+  var newIndex = (index + direction) % (collection.length);
+  return collection.eq(newIndex); // to get jQuery object rather than plain DOM element
+}
+// index, [slide] -> slide
+function nextSlide(elements, startIndex) {
+  'use strict';
+  return moveInCircle(elements, startIndex, 1);
+}
+
+// index, [slide] -> slide
+function prevSlide(elements, startIndex) {
+  'use strict';
+  return moveInCircle(elements, startIndex, -1);
+}
+
 function transition(oldSlide, newSlide) {
   'use strict';
-  [newSlide, oldSlide].map(function(elt) {
+  [newSlide, oldSlide].forEach(function(elt) {
     elt.fadeToggle('slow').toggleClass('selected');
   });
 }
 
 function transitionTowards(slides, currentSlide, direction, timeout) {
+  'use strict';
   var position = currentSlide.index(),
     target = (direction < 0 ? prevSlide(slides, position) : nextSlide(slides, position));
   timeout.restart(); // this is convenient but doesn't really belong here
@@ -23,32 +45,15 @@ $(document).ready(function() {
       },
       restart: function() {
         window.clearTimeout(this.timeoutID);
-        this.timeoutID = window.setTimeout(() => { this.action(); }, this.delay); // arrow function lexically binds this; otherwise setTimeout would set it to the global object
+        this.timeoutID = window.setTimeout(() => this.action(), this.delay); // arrow function lexically binds this; otherwise setTimeout would set it to the global object
       }
     };
   timeout.restart();
 
   $('.arrow-container').on('click', function() {
     var current = $('.selected'),
-      position = current.index(),
-      direction = $(this).attr('id') == 'right' ? 1 : -1;
+      direction = $(this).attr('id') === 'right' ? 1 : -1;
     transitionTowards(slides, current, direction, timeout);
   });
 });
 
-// Non-jQuery stuff below
-
-// [slide], int, int -> slide; direction will be +/- 1
-function moveInCircle(collection, index, direction) {
-  var newIndex = (index + direction) % (collection.length);
-  return collection.eq(newIndex); // to get jQuery object rather than plain DOM element
-}
-// index, [slide] -> slide
-function nextSlide(elements, startIndex) {
-  return moveInCircle(elements, startIndex, 1);
-}
-
-// index, [slide] -> slide
-function prevSlide(elements, startIndex) {
-  return moveInCircle(elements, startIndex, -1);
-}
