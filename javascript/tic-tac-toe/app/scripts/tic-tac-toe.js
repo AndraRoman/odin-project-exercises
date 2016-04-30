27816f46444f833c1f99d88410b7711357ff348d
Cathy Douglass <cdouglass256@gmail.com>
2016-04-29 19:46:11 -0700

    JS Tic-Tac-Toe: Alternate player; allow setting tile text exactly once

--- javascript/tic-tac-toe/app/scripts/tic-tac-toe.js
+++ javascript/tic-tac-toe/app/scripts/tic-tac-toe.js
@@ -8,6 +8,11 @@ var ticTacToe = {
     return tile;
   },
 
+  available: function(tile) {
+    var result = tile.jquery && tile.text().length == 0;
+    return result;
+  },
+
   newBoard: function (boardElt) {
     'use strict';
     var x,
@@ -29,9 +34,25 @@ var ticTacToe = {
 
   play: function (boardElt) {
     'use strict';
-    $('#board').removeClass('js-off');
-    var tileMatrix = this.newBoard(boardElt);
-    // TODO gameplay
+    boardElt.removeClass('js-off');
+    var tileMatrix = this.newBoard(boardElt),
+      available = this.available,
+      nand = function(a, b) {
+        return !(a && b);
+      },
+      lastTileClicked = boardElt.asEventStream('click').map(function(event) {
+        return $(event.target);
+      }).toProperty("").filter(available),
+      toggle = function(x) { return !x},
+      boolToPlayer = function(x) { return x ? 'x' : 'o' },
+      oddTurn = lastTileClicked.scan(false, toggle),
+      currentPlayer = oddTurn.map(boolToPlayer);
+    currentPlayer.assign($('#current-player'), 'text');
+    lastTileClicked.onValue(function(v) {
+      var alternator = lastTileClicked.scan(false, toggle),
+        nanded = Bacon.combineWith(nand, alternator, oddTurn);
+      nanded.take(1).map(boolToPlayer).assign($(v), 'text'); // nanded.take(n) creates a stream that takes at most n values from nanded and then ends
+    });
   }
 };
 
