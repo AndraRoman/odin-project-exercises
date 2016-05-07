4c10877079b2cd19d8f2212beab51d73588fd3b3
Cathy Douglass <cdouglass256@gmail.com>
2016-05-07 11:37:26 -0700

    JS Tic-Tac-Toe: Clean up code

--- javascript/tic-tac-toe/app/scripts/tic-tac-toe.js
+++ javascript/tic-tac-toe/app/scripts/tic-tac-toe.js
@@ -5,9 +5,7 @@ var ticTacToe = {
 
   newBoard: function (boardElt) {
     'use strict';
-    var x,
-      y,
-      row;
+    var x, y, row;
     for (x = 0; x < utils.width; x += 1) {
       row = $('<div/>').addClass('row'); // handy shortcut!
       boardElt.append(row);
@@ -28,57 +26,47 @@ var ticTacToe = {
 
   play: function (boardElt) {
     'use strict';
-    var findWinsByPlayer,
-      oddWins,
-      evenWins,
-      catsGame,
-      gameOver,
-      rawLastTileClicked = boardElt.asEventStream('click')
-        .map(function(event) { return utils.getTileCoords($(event.target)); })
-        .toProperty(),
-      allTilesClicked = rawLastTileClicked
-        .scan([], function(arr, point) {
-          return point.addToSet(arr); })
-        .skipDuplicates(function(arr1, arr2) {
-          var newVector = arr2[arr2.length - 1];
-          return arr1.length > 0 && newVector.memberOf(arr1);
-          }),
-      lastTileMarked = allTilesClicked.skip(1).map(function(arr) { return arr[arr.length - 1]; }),
-      oddTurn = lastTileMarked.scan(true, utils.not),
+    var checkWin,
+      gameWon,
+      boardFull,
+      allTilesClicked = boardElt.asEventStream('click')
+        .map(function (event) { return utils.getTileCoords($(event.target)); }).toProperty()
+        .scan([], function (arr, point) { return point.addToSet(arr); })
+        .skipDuplicates(function (arr1, arr2) { return arr1.length === arr2.length; }),
+      lastTileMarked = allTilesClicked.skip(1).map(function (arr) { return arr[arr.length - 1]; }),
+      oddTurn = lastTileMarked.scan(true, function (x) { return !x; }),
       tilesOdd = lastTileMarked.filter(oddTurn)
-        .scan([], function(arr, obj) { return arr.concat([obj]); }),
+        .scan([], function (arr, obj) { return arr.concat([obj]); }),
       tilesEven = lastTileMarked.filter(oddTurn.not())
-        .scan([], function(arr, obj) { return arr.concat([obj]); });
+        .scan([], function (arr, obj) { return arr.concat([obj]); });
 
     oddTurn.map(function (b) { return b ? 'x' : 'o'; }).assign($('#current-player'), 'text');
     tilesOdd.onValue(utils.draw(boardElt, 'o'));
     tilesEven.onValue(utils.draw(boardElt, 'x'));
 
-    findWinsByPlayer = function(arr) {
+    checkWin = function (arr) {
        var point = arr[arr.length - 1],
         rest = arr.slice(0, arr.length - 1),
         winningTiles;
-      if (arr.length < 1) { return false; } else {
-        winningTiles = point.wins(rest);
-        if (winningTiles.length > 0) { return winningTiles; } else { return false; }
-      }
+      if (arr.length < 1) { return false; }
+      winningTiles = point.wins(rest);
+      if (winningTiles.length > 0) { return winningTiles; } else { return false; }
     };
 
-    catsGame = allTilesClicked.map(function (arr) { return arr.length >= utils.width * utils.width; }).skipDuplicates().skip(1).startWith(false);
-    oddWins = tilesOdd.map(findWinsByPlayer);
-    evenWins = tilesEven.map(findWinsByPlayer);
+    boardFull = allTilesClicked.map(function (arr) { return arr.length >= utils.width * utils.width; })
+      .skipDuplicates().skip(1).startWith(false);
 
-    oddWins.or(evenWins).filter(utils.ident).onValue(function(arr) {
-      var i,
-        color = function (point) { utils.findTile(boardElt, point).addClass('won'); };
-      for (i = 0; i < arr.length; i += 1) {
-        arr.forEach(color);
-      }
-    });
+    gameWon = tilesOdd
+      .combine(tilesEven, function (a, b) { return checkWin(a) || checkWin(b); }); 
 
-    gameOver = oddWins.or(evenWins).or(catsGame).filter(utils.ident);
+    gameWon.filter(utils.ident).onValue(function (arr) {
+        var i, color = function (point) { utils.findTile(boardElt, point).addClass('won'); };
+        for (i = 0; i < arr.length; i += 1) {
+          arr.forEach(color);
+        }
+      });
 
-    gameOver.onValue(function () {
+    gameWon.or(boardFull).filter(utils.ident).onValue(function () {
       $('p:first-of-type').css('visibility', 'hidden');
       $('.tile').addClass('tile-frozen').removeClass('tile');
     });
--- javascript/tic-tac-toe/app/scripts/utils.js
+++ javascript/tic-tac-toe/app/scripts/utils.js
@@ -7,62 +7,11 @@ var utils = {
 
   ident: function (val) { 'use strict'; return val; },
 
-  Vector: function Vector(x, y) {
-    'use strict';
-    var self = this;
-    this.equals = function (other) {
-      return (other.x === x) && (other.y === y);
-    };
-    this.x = x;
-    this.y = y;
-    this.memberOf = function (arr) {
-      return arr.reduce(function (a, b) { return a || self.equals(b); }, false);
-    };
-    this.addToSet = function (arr) {
-      if (self.memberOf(arr)) {
-        return arr;
-      }
-      return arr.concat(self);
-    };
-    this.add = function (diff) {
-      return new Vector(x + diff.x, y + diff.y);
-    };
-    this.negate = function () {
-      return new Vector(-x, -y);
-    };
-    this.wins = function (arr) {
-      var nextPoint = arr[0],
-        rest = arr.slice(1),
-        diff,
-        distance,
-        lastPoint;
-      if (arr.length < 1) { return false; }
-      diff = this.negate().add(nextPoint);
-      distance = Math.max(Math.abs(diff.x), Math.abs(diff.y));
-
-      if (distance > 1) {
-        if (diff.x % distance === 0 && diff.y % distance === 0) {
-          diff.x = diff.x / distance;
-          diff.y = diff.y / distance;
-        }
-      }
-
-      // TODO don't be specific to width of 3
-      lastPoint = [nextPoint.add(diff), nextPoint.add(diff.negate()), this.add(diff.negate()), this.add(diff)]
-              .filter(function (point) { return (point.memberOf(rest)); })[0];
-      if (lastPoint) { return [this, nextPoint, lastPoint]; }
-      return this.wins(rest);
-    };
-  },
-
-  not: function (x) {'use strict'; return !x; },
-
   getTileCoords: function (tile) {
     'use strict';
     var x = tile.index(),
-      y = tile.parent().index(),
-      p = new this.Vector(x, y);
-    return p;
+      y = tile.parent().index();
+    return new this.Vector(x, y);
   },
 
   findTile: function findTile(board, point) {
@@ -75,13 +24,56 @@ var utils = {
     var board = $(elt),
       self = this;
     return function (points) {
-      var i, p;
+      var i;
       for (i = 0; i < points.length; i += 1) {
-        p = points[i];
-        self.findTile(board, p).addClass(symbol);
+        self.findTile(board, points[i]).addClass(symbol);
       }
     };
-  }
+  },
+
+  Vector: function Vector(x, y) {
+    'use strict';
+    var self = this;
+    this.x = x;
+    this.y = y;
+
+    this.equals = function (other) { return (other.x === x) && (other.y === y); };
+
+    this.memberOf = function (arr) {
+      return arr.reduce(function (a, b) { return a || self.equals(b); }, false);
+    };
+
+    this.addToSet = function (arr) {
+      if (self.memberOf(arr)) { return arr; }
+      return arr.concat(self);
+    };
+
+    this.add = function (diff) { return new Vector(x + diff.x, y + diff.y); };
+
+    this.negate = function () { return new Vector(-x, -y); };
+
+    this.wins = function (arr) {
+      var nextPoint = arr[0],
+        rest = arr.slice(1),
+        diff,
+        distance,
+        lastPoint;
+      if (arr.length < 1) { return false; }
+      diff = this.negate().add(nextPoint);
+      distance = Math.max(Math.abs(diff.x), Math.abs(diff.y));
+
+      if (distance > 1 && diff.x % distance === 0 && diff.y % distance === 0) {
+        diff.x = diff.x / distance;
+        diff.y = diff.y / distance;
+      }
+
+      // TODO don't be specific to width of 3
+      lastPoint = [nextPoint.add(diff), nextPoint.add(diff.negate()), this.add(diff.negate()), this.add(diff)]
+        .filter(function (point) { return (point.memberOf(rest)); })[0];
+      if (lastPoint) { return [this, nextPoint, lastPoint]; }
+      return this.wins(rest);
+    };
+  },
 };
 
 if (typeof module !== 'undefined' && typeof (module.exports) !== 'undefined') {
@@ -89,4 +81,3 @@ if (typeof module !== 'undefined' && typeof (module.exports) !== 'undefined') {
     utils: utils
   };
 }
-
